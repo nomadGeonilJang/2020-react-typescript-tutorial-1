53ec8d536aa96b4c02d5fa1f9b0e5d8f4a6e7434
JangGeonil <wkdrjsdlf2@gmail.com>
2020-10-20 01:06:47 +0900

    refactoring counter with useReducer

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
+
+type Action = { type: "INCREASE" } | { type: "DECREASE" };
+
+function reducer(state: number, action: Action): number {
+  if (action.type === "INCREASE") {
+    return state + 1;
+  }
+  if (action.type === "DECREASE") {
+    if (state) {
+      return state - 1;
+    }
+    return 0;
+  }
+  throw new Error("Unhandled action type");
+}
 
 function Counter() {
-  const [count, setCount] = useState(0);
-  const onIncrease = () => setCount((pre) => pre + 1);
-  const onDecrease = () => setCount((pre) => (pre > 0 ? pre - 1 : pre));
+  const [state, dispatch] = useReducer(reducer, 0);
+  const onIncrease = () => dispatch({ type: "INCREASE" });
+  const onDecrease = () => dispatch({ type: "DECREASE" });
   return (
     <div>
-      <h1>{count}</h1>
+      <h1>{state}</h1>
       <button onClick={onIncrease}>+1</button>
       <button onClick={onDecrease}>-1</button>
     </div>
